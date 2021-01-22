d797618fdd35146e3a020ab51b0fe6100f2f4380
Abid Nuri <mdabidnuri@gmail.com>
2021-01-22 09:57:59 +0600

    budgetCalculator

--- assignment.js
+++ assignment.js
@@ -5,9 +5,31 @@ function kilometerToMeter(kilometer) {
     if (kilometer < 0) {
         return "Please use positive number."
     }
-    else{
+    else {
         return kilometer;
     }
 }
-var meter = kilometerToMeter(1);
+var meter = kilometerToMeter(8);
 console.log(meter);
+
+// Second Problem budgetCalculator
+// WatchPrice = 50, PhonePrice = 100, LaptopPrice = 500
+function budgetCalculator(watch, phone, laptop) {
+    if (watch < 0) {
+        return "Error! Put The Right Value Of Watch."
+    }
+    else if (phone < 0) {
+        return "Error! Put The Right Value Of Phone."
+    }
+    else if (laptop < 0) {
+        return "Error! Put The Right Value Of Laptop."
+    }
+    else {
+        var total = watch * 50 + phone * 100 + laptop * 500;
+        return total;
+    }
+}
+var result = budgetCalculator(1, 2, 1);
+console.log(result);
+
+
